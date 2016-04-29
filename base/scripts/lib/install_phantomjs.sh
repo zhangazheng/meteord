aaa2cd16360bce5505f38568e00ebfe0c1a9745e
zhangazheng <lovezhang.zheng@gmail.com>
2016-04-29 09:43:26 +0800

    update

--- base/scripts/lib/install_phantomjs.sh
+++ base/scripts/lib/install_phantomjs.sh
@@ -5,8 +5,9 @@ ARCH=`uname -m`
 PHANTOMJS_VERSION=1.9.8
 PHANTOMJS_TAR_FILE=phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
 
+cp $METEORD_DIR/lib/phantomjs-1.9.8-linux-x86_64.tar.bz2 /usr/local/share/
 cd /usr/local/share/
-curl -L -O https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
+#curl -L -O https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
 tar xjf $PHANTOMJS_TAR_FILE
 ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/share/phantomjs
 ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/bin/phantomjs
