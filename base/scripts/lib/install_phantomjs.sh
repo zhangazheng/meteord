c7de8b99972aaf716453bc764fd736b431abc8ea
zhangzheng <lovezhang.zheng@gmail.com>
2018-04-12 15:33:44 +0800

    1.6.1 package prepare

--- base/scripts/lib/install_phantomjs.sh
+++ base/scripts/lib/install_phantomjs.sh
@@ -2,10 +2,10 @@
 set -e
 apt-get -y install libfreetype6 libfreetype6-dev fontconfig
 ARCH=`uname -m`
-PHANTOMJS_VERSION=1.9.8
+PHANTOMJS_VERSION=2.1.1
 PHANTOMJS_TAR_FILE=phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
 
-cp $METEORD_DIR/lib/phantomjs-1.9.8-linux-x86_64.tar.bz2 /usr/local/share/
+cp $METEORD_DIR/lib/${PHANTOMJS_TAR_FILE} /usr/local/share/
 cd /usr/local/share/
 #curl -L -O https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
 tar xjf $PHANTOMJS_TAR_FILE
